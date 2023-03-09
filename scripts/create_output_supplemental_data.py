1a84c3c42af6f63077ab24ac9d6672497349c799
Nellie McKesson <nelliemckesson@gmail.com>
2023-03-08 20:34:04 -0800

    cleaning up a bit

--- scripts/create_output_supplemental_data.py
+++ scripts/create_output_supplemental_data.py
@@ -5,14 +5,14 @@ import sys
 import json
 import re
 
-def add_release_version(doxyfile_path, data_obj):
+def get_release_version(doxyfile_path):
+	version = "unknown"
 	with open(doxyfile_path) as f:
 		doxy_content = f.read()
 	version_search = re.search("(\nPROJECT_NUMBER\s*=\s*)([\d.]+)", doxy_content)
 	if version_search is not None:
 		version = version_search.group(2)
-		data_obj["pico_sdk_release"] = version
-	return data_obj
+	return version
 
 def write_new_data_file(output_dir, data_obj):
 	f = open(output_dir, 'w')
@@ -22,9 +22,9 @@ def write_new_data_file(output_dir, data_obj):
 
 if __name__ == "__main__":
 	# read the doxygen config file
-	input_file = sys.argv[1]
+	doxyfile_path = sys.argv[1]
 	# output the new data file
 	output_dir = sys.argv[2]
-	data_obj = {}
-	data_obj = add_release_version(input_file, data_obj)
+	version = get_release_version(doxyfile_path)
+	data_obj = {"pico_sdk_release": version}
 	write_new_data_file(output_dir, data_obj)
