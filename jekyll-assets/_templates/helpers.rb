06de0d78a671134f7fb1099d325191ad4cd5772f
Nellie McKesson <nelliemckesson@gmail.com>
2023-08-03 12:38:22 -0700

    getting first draft working

--- jekyll-assets/_templates/helpers.rb
+++ jekyll-assets/_templates/helpers.rb
@@ -1,16 +1,14 @@
 module Slim::Helpers
   def book_link
-    puts (self.attr 'booktype')
-    puts (document.attr 'booktype')
     case (self.attr 'booktype')
     when 'free'
-      %("FREEEEEEE")
+      %(You can <a href="#{self.attr 'link'}" target="_blank">download this book</a> as a PDF file for free, it has been released under a Creative Commons <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">Attribution-NonCommercial-ShareAlike</a> 3.0 Unported (CC BY NC-SA) licence.)
     when 'buy'
-      %("BUY TEXT")
+      %(You can <a href="#{self.attr 'link'}" target="_blank">buy this book</a> on the Raspberry Pi Press site.)
     when 'donate'
-      %("DONATE")
+      %(You can <a href="#{document.attr 'link'}" target="_blank">download this book</a> for an optional donation on the Raspberry Pi Press site.)
     else
-      %("NONE")
+      return
     end
   end
 
