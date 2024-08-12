cbfff7d7b67821f80cb50e943e10aa810eeaf49a
Andrew Scheller <lurch@durge.org>
2024-08-12 17:52:15 +0100

    Standardise whitespace and shebang in Python scripts

--- scripts/create_output_supplemental_data.py
+++ scripts/create_output_supplemental_data.py
@@ -6,24 +6,24 @@ import json
 import re
 
 def get_release_version(doxyfile_path):
-	version = "unknown"
-	with open(doxyfile_path) as f:
-		doxy_content = f.read()
-	version_search = re.search(r"(\nPROJECT_NUMBER\s*=\s*)([\d.]+)", doxy_content)
-	if version_search is not None:
-		version = version_search.group(2)
-	return version
+    version = "unknown"
+    with open(doxyfile_path) as f:
+        doxy_content = f.read()
+    version_search = re.search(r"(\nPROJECT_NUMBER\s*=\s*)([\d.]+)", doxy_content)
+    if version_search is not None:
+        version = version_search.group(2)
+    return version
 
 def write_new_data_file(output_json_file, data_obj):
-	f = open(output_json_file, 'w')
-	f.write(json.dumps(data_obj))
-	f.close()
+    f = open(output_json_file, 'w')
+    f.write(json.dumps(data_obj))
+    f.close()
 
 if __name__ == "__main__":
-	# read the doxygen config file
-	doxyfile_path = sys.argv[1]
-	# output the new data file
-	output_json_file = sys.argv[2]
-	version = get_release_version(doxyfile_path)
-	data_obj = {"pico_sdk_release": version}
-	write_new_data_file(output_json_file, data_obj)
+    # read the doxygen config file
+    doxyfile_path = sys.argv[1]
+    # output the new data file
+    output_json_file = sys.argv[2]
+    version = get_release_version(doxyfile_path)
+    data_obj = {"pico_sdk_release": version}
+    write_new_data_file(output_json_file, data_obj)
