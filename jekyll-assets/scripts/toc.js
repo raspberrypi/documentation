d3b7019bce2d7483da30056f5b8e774ad0389abe
Nate Contino <nate.contino@raspberrypi.com>
2024-12-03 18:18:01 +0000

    Refactored the toc and theme javascript into separate files; cleaned up some other files

---
+++ jekyll-assets/scripts/toc.js
@@ -0,0 +1,33 @@
+/* Trigger Tocbot dynamic TOC, works with tocbot 3.0.2 */
+function makeToc() {
+var oldtoc = document.getElementById('toc').nextElementSibling;
+var newtoc = document.getElementById('tocbot');
+newtoc.setAttribute('id', 'tocbot');
+newtoc.setAttribute('class', 'js-toc');
+oldtoc.parentNode.replaceChild(newtoc, oldtoc);
+tocbot.init({ contentSelector: '#content',
+    headingSelector: 'h1, h2, h3, h4',
+    smoothScroll: true,
+    includeHtml: true
+});
+var handleTocOnResize = function() {
+    var width = window.innerWidth
+                || document.documentElement.clientWidth
+                || document.body.clientWidth;
+    if (width < 768) {
+        tocbot.refresh({ contentSelector: '#content',
+            headingSelector: 'h1, h2, h3, h4',
+            collapseDepth: 6,
+            activeLinkClass: 'ignoreactive',
+            throttleTimeout: 1000,
+            smoothScroll: true });
+    }
+    else {
+        tocbot.refresh({ contentSelector: '#content',
+            headingSelector: 'h1, h2, h3, h4',
+            smoothScroll: true });
+    }
+};
+window.addEventListener('resize', handleTocOnResize);
+handleTocOnResize();
+}
