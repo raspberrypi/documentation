cb1bd369f525f9a8788ef54008abf8a2b46ac3f1
Nate Contino <nate.contino@raspberrypi.com>
2024-12-09 12:16:09 +0000

    Incorporate updated rpi header and footer; improve footer appearance; slight isual tweaks

--- jekyll-assets/scripts/theme.js
+++ jekyll-assets/scripts/theme.js
@@ -1,13 +1,19 @@
 // toggles the site theme between dark and light
 function toggleTheme() {
+  var cookiePermission = document.cookie.indexOf("cookiebanner_accepted");
+  
   // fetch the theme from local storage (if it exists)
   var theme = localStorage.getItem('theme');
   // if the theme has never been set, or is light, set the theme to the dark symbol in local storage to change it
   if (localStorage.getItem('theme') !== null && theme == '🌝' ) {
-    localStorage.setItem('theme', '🌚');
+    if (document.cookie.indexOf("cookiebanner_accepted") != -1) {
+      localStorage.setItem('theme', '🌚');
+    }
   } else {
-    // otherwise, the theme is currently set to dark, so set the theme to the light symbol in local storage to change it
-    localStorage.setItem('theme', '🌝');
+    if (document.cookie.indexOf("cookiebanner_accepted") != -1) {
+      // otherwise, the theme is currently set to dark, so set the theme to the light symbol in local storage to change it
+      localStorage.setItem('theme', '🌝');
+    }
   }
   // finally, toggle the light option off or on the body to change the display of the theme
   document.body.classList.toggle('light');
@@ -19,14 +25,17 @@ function toggleTheme() {
 // from the site's perspective, we default to a dark theme, but toggle it to a light theme on load if the user doesn't ask for dark.
 // why do this? To prevent an annoying light 'flash' for dark theme users. light theme users don't really notice or care if there's a dark anti-flash.
 function initTheme() {
-  // fetch the theme from local storage (if it exists)
-  var theme = localStorage.getItem('theme');
-  // if the theme has been set to light (null check to short circuit if not set)
-  if(theme !== null && theme === '🌝'
-    // if we can use matchMedia and the browser supports the dark color scheme
-    || (window.matchMedia && !window.matchMedia('(prefers-color-scheme: dark)').matches)
-    && theme !== '🌚') {
-      // toggles the theme from the default dark mode to the light version (which actually _shows_ by default to many users)
-      document.body.classList.toggle('light');
+  var cookiePermission = document.cookie.indexOf("cookiebanner_accepted");
+  if (document.cookie.indexOf("cookiebanner_accepted") != -1) {
+    // fetch the theme from local storage (if it exists)
+    var theme = localStorage.getItem('theme');
+    // if the theme has been set to light (null check to short circuit if not set)
+    if(theme !== null && theme === '🌝'
+      // if we can use matchMedia and the browser supports the dark color scheme
+      || (window.matchMedia && !window.matchMedia('(prefers-color-scheme: dark)').matches)
+      && theme !== '🌚') {
+        // toggles the theme from the default dark mode to the light version (which actually _shows_ by default to many users)
+        document.body.classList.toggle('light');
+    }
   }
 }
