673a53cbabac96d00ec56a21d2f2cbbb8f93dd51
Nate Contino <nate.contino@raspberrypi.com>
2024-12-10 12:01:15 +0000

    Improve toc element alignment, boxes landing page search bar appearance, fix toc error on landing page

--- jekyll-assets/scripts/toc.js
+++ jekyll-assets/scripts/toc.js
@@ -1,33 +1,36 @@
 /* Trigger Tocbot dynamic TOC, works with tocbot 3.0.2 */
 function makeToc() {
-var oldtoc = document.getElementById('toc').nextElementSibling;
-var newtoc = document.getElementById('tocbot');
-newtoc.setAttribute('id', 'tocbot');
-newtoc.setAttribute('class', 'js-toc');
-oldtoc.parentNode.replaceChild(newtoc, oldtoc);
-tocbot.init({ contentSelector: '#content',
-    headingSelector: 'h1, h2, h3, h4',
-    smoothScroll: true,
-    includeHtml: true
-});
-var handleTocOnResize = function() {
-    var width = window.innerWidth
-                || document.documentElement.clientWidth
-                || document.body.clientWidth;
-    if (width < 768) {
-        tocbot.refresh({ contentSelector: '#content',
+    var tocElement = document.getElementById('toc');
+    if (tocElement) { /* only run on pages that have a toc -- exclude boxes landing pages, for instance */
+        var oldtoc = tocElement.nextElementSibling;
+        var newtoc = document.getElementById('tocbot');
+        newtoc.setAttribute('id', 'tocbot');
+        newtoc.setAttribute('class', 'js-toc');
+        oldtoc.parentNode.replaceChild(newtoc, oldtoc);
+        tocbot.init({ contentSelector: '#content',
             headingSelector: 'h1, h2, h3, h4',
-            collapseDepth: 6,
-            activeLinkClass: 'ignoreactive',
-            throttleTimeout: 1000,
-            smoothScroll: true });
+            smoothScroll: true,
+            includeHtml: true
+        });
+        var handleTocOnResize = function() {
+            var width = window.innerWidth
+                        || document.documentElement.clientWidth
+                        || document.body.clientWidth;
+            if (width < 768) {
+                tocbot.refresh({ contentSelector: '#content',
+                    headingSelector: 'h1, h2, h3, h4',
+                    collapseDepth: 6,
+                    activeLinkClass: 'ignoreactive',
+                    throttleTimeout: 1000,
+                    smoothScroll: true });
+            }
+            else {
+                tocbot.refresh({ contentSelector: '#content',
+                    headingSelector: 'h1, h2, h3, h4',
+                    smoothScroll: true });
+            }
+        };
+        window.addEventListener('resize', handleTocOnResize);
+        handleTocOnResize();
     }
-    else {
-        tocbot.refresh({ contentSelector: '#content',
-            headingSelector: 'h1, h2, h3, h4',
-            smoothScroll: true });
-    }
-};
-window.addEventListener('resize', handleTocOnResize);
-handleTocOnResize();
 }
